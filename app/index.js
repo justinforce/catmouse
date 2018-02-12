05c6136f8577617e6a1656e8002edc68a1c4441f
Justin Force <justin.force@gmail.com>
2018-02-11 22:52:07 -0800

    Get a basic loop bouncing a ball around
    And do it in a very functional style. Feels very nice.

--- app/index.js
+++ app/index.js
@@ -6,9 +6,11 @@
  *    Simple data structures and functions should be good enough.
  * 2. Favor a functional style. Avoid side effects as far as is practical.
  * 3. When side effects are required, group them all at the end of the function.
+ * 4. Favor vectors with property names x/y as it will encourage generic
+ *    functions.
  */
 
-// import { assoc } from 'ramda';
+import { clamp, times } from 'ramda';
 import './style.css';
 
 // Width of the world
@@ -21,46 +23,115 @@ const height = 600;
 const fps = 60;
 
 // How much time to simulate each update
-const timeStep = 1000 / fps;
+const stepLength = 1000 / fps;
 
 // The canvas element where the world is drawn
 const canvas = document.querySelector('canvas');
-// const ctx = canvas.getContext('2d');
+
+// The rendering context
+const ctx = canvas.getContext('2d');
+
+const ball = {
+  speed: {
+    x: 0.15,
+    y: 0.15,
+  },
+  box: {
+    x: 10,
+    y: 10,
+  },
+};
+
+const world = {
+  box: {
+    x: width,
+    y: height,
+  },
+};
+
+// All mutable state is stored here
+const st = {
+  ball: {
+    pos: {
+      x: (width / 2) - ball.box.x,
+      y: (height / 2) - ball.box.y,
+    },
+    vel: {
+      x: ball.speed.x,
+      y: ball.speed.y,
+    },
+  },
+};
+
+// Negates val if test is false, otherwise return val
+const negateIf = (test, val) => (test ? -val : val);
+
+// Returns true if val is more than, less than, or equal to either low or high
+const outOfRangeExclusive = (low, high, val) => val <= low || val >= high;
 
 /**
- * Updates the simulation
+ * Updates the simulation for a given time interval dt.
  */
-function update() {
-}
+const update = dt => () => {
+  // update ball
+  (() => {
+    // FIXME There's a better way to do this than writing axis a billion times.
+    const worldBoundary = axis => world.box[axis];
+    const edgeOfWorld = axis =>
+      outOfRangeExclusive(ball.box.x, worldBoundary(axis), st.ball.pos[axis]);
+    const velocity = axis => negateIf(edgeOfWorld(axis), st.ball.vel[axis]);
+    const velocityX = velocity('x');
+    const velocityY = velocity('y');
+    const incrementalVelocity = axis => st.ball.pos[axis] + (velocity(axis) * dt);
+    const position = axis =>
+      clamp(0, worldBoundary(axis), incrementalVelocity(axis));
+    const positionX = position('x');
+    const positionY = position('y');
+
+    // side effects
+    st.ball.vel.x = velocityX;
+    st.ball.vel.y = velocityY;
+    st.ball.pos.x = positionX;
+    st.ball.pos.y = positionY;
+  })();
+};
 
 /**
  * Draws the frame
  */
-function draw() {
-}
+const draw = () => {
+  // draw background
+  ctx.fillStyle = '#333';
+  ctx.fillRect(0, 0, width, height);
+
+  // draw ball
+  (() => {
+    const x = st.ball.pos.x - (ball.box.x / 2);
+    const y = st.ball.pos.y - (ball.box.y / 2);
+    const radius = ball.box.x / 2;
+    ctx.fillStyle = '#efefef';
+    ctx.strokeStyle = '#333';
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fill();
+  })();
+};
 
 /**
- * Performs all of the updates required since the last frame, then draws the
- * frame.
- *
- * FIXME Relies on external variable timeStep
- * FIXME Mutates state of dt
+ * Returns a function that runs as many updates as are needed for the given
+ * timestamp and accumulated unsimulated time, draws the frame, then enqueues
+ * the next step on the next animation frame.
  */
-function step(dt0, lastFrame) {
-  return (now) => {
-    let dt;
-    for (dt = dt0 + (now - lastFrame); dt >= timeStep; dt -= timeStep) {
-      update();
-    }
-    draw();
-    requestAnimationFrame(step(dt, now));
-  };
-}
+const step = (t0, accumulatedTime) => (t) => {
+  const dt = accumulatedTime + (t - t0);
+  const requiredUpdateCount = Math.floor(dt / stepLength);
+  const leftoverTime = dt % stepLength;
+  times(update(dt), requiredUpdateCount);
+  draw();
+  requestAnimationFrame(step(t, leftoverTime));
+};
 
-function init() {
-  canvas.width = width;
-  canvas.height = height;
-  requestAnimationFrame(step(0, 0));
-}
-
-init();
+// init
+canvas.width = width;
+canvas.height = height;
+requestAnimationFrame(step(0, 0));
