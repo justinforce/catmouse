b4e9817cce584b48f9fe5b7b56e8e0f000256f77
Justin Force <justin.force@gmail.com>
2018-08-22 14:55:21 -0700

    Simplify AI early return logic

--- app/ai.js
+++ app/ai.js
@@ -7,10 +7,11 @@ const delta = (state) => {
   const {
     ball, input, player,
   } = state;
+  if (!input.ai) return state;
   const playerBallAngle = angleBetween(player.positions[0], ball.position);
   const playerBallOffset = player.velocity[1] - playerBallAngle;
-  const left = input.ai ? playerBallOffset > 0 : input.left;
-  const right = input.ai ? playerBallOffset < 0 : input.right;
+  const left = playerBallOffset > 0;
+  const right = playerBallOffset < 0;
   return mergeDeepRight(state, {
     input: {
       left,
